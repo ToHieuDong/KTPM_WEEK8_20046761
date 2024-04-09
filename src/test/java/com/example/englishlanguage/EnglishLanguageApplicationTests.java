7b1180f2aced9274206990e7b037d019dd606f63
ToHieuDong <hieudong.dongthanh.02@gmail.com>
2024-04-10 00:01:08 +0700

    commit

---
+++ src/test/java/com/example/englishlanguage/EnglishLanguageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.englishlanguage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishLanguageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
